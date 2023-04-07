a6b8c20edf0c3e4269507a6a365da176236ac813
Bikara950225 <2589116399@qq.com>
2023-04-07 19:48:24 +0800

    chore(): 提交code62实现

--- code62/main.go
+++ code62/main.go
@@ -7,16 +7,23 @@ func uniquePaths(m int, n int) int {
 	for i := range dp {
 		dp[i] = make([]int, n)
 	}
+
 	for i := 0; i < m; i++ {
 		for j := 0; j < n; j++ {
 			if i == 0 || j == 0 {
 				dp[i][j] = 1
-				continue
+			} else {
+				dp[i][j] = dp[i-1][j] + dp[i][j-1]
 			}
-			dp[i][j] = dp[i-1][j] + dp[i][j-1]
 		}
 	}
+
 	return dp[m-1][n-1]
+}
+
+// 用深度优先来实现，会超时，写来找感觉
+func uniquePathsWithDfs(m int, n int) int {
+	return 0
 }
 
 func main() {
